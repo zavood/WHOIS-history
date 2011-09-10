87b1a21918b4363582322eeb87e641804b55aaa6
xml <xml@sandbox-test1.(none)>
2011-09-10 15:46:24 -0400

    auto checkin

---
+++ pl/promaster/promaster.pl
@@ -0,0 +1,23 @@
+
+DOMAIN NAME:           promaster.pl
+registrant type:       organization
+nameservers:           dns3.home.pl. [81.210.44.122]
+                       dns.home.pl. [62.129.252.30]
+                       dns2.home.pl. [62.129.252.40]
+created:               2004.11.30 16:28:26
+last modified:         2010.11.17 09:42:51
+
+no option
+
+
+REGISTRAR:
+Home.pl sp.j.
+pl. Rodla 9
+70-419 Szczecin
+Polska/Poland
++48.914325555
++48.801445555
+info@home.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
