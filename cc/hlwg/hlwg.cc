8ea80d7ae97485e7eec2233d2f4d1d752d12db11
xml <xml@sandbox-test1.(none)>
2011-09-11 17:11:26 -0400

    auto checkin

---
+++ cc/hlwg/hlwg.cc
@@ -0,0 +1,136 @@
+
+Whois Server Version 1.0
+
+Domain names can now be registered with many different competing registrars.
+Go to http://registrar.verisign-grs.com/whois/ for detailed information.
+
+   Domain Name: HLWG.CC
+   Registrar: 1 API GMBH
+   Whois Server: whois.1api.net
+   Referral URL: http://www.1api.net
+   Name Server: NS231.DNSEVER.COM
+   Name Server: NS259.DNSEVER.COM
+   Name Server: NS77.DNSEVER.COM
+   Name Server: NS38.DNSEVER.COM
+   Name Server: NS91.DNSEVER.COM
+   Status: ACTIVE
+   Updated Date: 22-may-2011
+   Creation Date: 22-may-2011
+   Expiration Date: 22-may-2014
+
+
+&gt;&gt;&gt; Last update of whois database: Sun, 11 Sep 2011 13:22:48 EDT &lt;&lt;&lt;
+
+NOTICE: The expiration date displayed in this record is the date the
+registrar's sponsorship of the domain name registration in the registry is
+currently set to expire. This date does not necessarily reflect the
+expiration date of the domain name registrant's agreement with the
+sponsoring registrar.  Users may consult the sponsoring registrar's
+Whois database to view the registrar's reported date of expiration
+for this registration.
+
+TERMS OF USE: You are not authorized to access or query our Whois
+database through the use of electronic processes that are high-volume and
+automated except as reasonably necessary to register domain names or
+modify existing registrations; the Data in VeriSign's (&quot;VeriSign&quot;) Whois
+database is provided by VeriSign for information purposes only, and to
+assist persons in obtaining information about or related to a domain name
+registration record. VeriSign does not guarantee its accuracy.
+By submitting a Whois query, you agree to abide by the following terms of
+use: You agree that you may use this Data only for lawful purposes and that
+under no circumstances will you use this Data to: (1) allow, enable, or
+otherwise support the transmission of mass unsolicited, commercial
+advertising or solicitations via e-mail, telephone, or facsimile; or
+(2) enable high volume, automated, electronic processes that apply to
+VeriSign (or its computer systems). The compilation, repackaging,
+dissemination or other use of this Data is expressly prohibited without
+the prior written consent of VeriSign. You agree not to use electronic
+processes that are automated and high-volume to access or query the
+Whois database except as reasonably necessary to register domain names
+or modify existing registrations. VeriSign reserves the right to restrict
+your access to the Whois database in its sole discretion to ensure
+operational stability.  VeriSign may restrict or terminate your access to the
+Whois database for failure to abide by these terms of use. VeriSign
+reserves the right to modify these terms at any time.
+
+The Registry database contains ONLY .cc, .tv, and .jobs domains
+and Registrars.
+; This data is provided by HEXONET Services Inc.
+; for information purposes, and to assist persons obtaining information
+; about or related to domain name registration records.
+; HEXONET Services Inc. does not guarantee its accuracy.
+; By submitting a WHOIS query, you agree that you will use this data
+; only for lawful purposes and that, under no circumstances, you will
+; use this data to
+; 1) allow, enable, or otherwise support the transmission of mass
+;    unsolicited, commercial advertising or solicitations via E-mail
+;    (spam); or
+; 2) enable high volume, automated, electronic processes that apply
+;    to this WHOIS server.
+; These terms may be changed without prior notice.
+; By submitting this query, you agree to abide by this policy.
+
+
+
+DOMAIN: HLWG.CC
+
+RSP: HEXONET Services Inc.
+URL: http://www.HEXONET.net
+
+created-date: 2011-05-22 11:21:17
+updated-date: 2011-05-22 11:29:32
+registration-expiration-date: 2014-05-22 11:21:18
+
+owner-organization: Fuck!
+owner-name: Fuck! Fuck!
+owner-street: Fuck!
+owner-city: Fuck!
+owner-state: Fuck!
+owner-zip: 110
+owner-country: DE
+owner-phone: +86.110
+owner-fax: +86.110
+owner-email: Your@Is.Sb
+
+admin-organization: Fuck!
+admin-name: Fuck! Fuck!
+admin-street: Fuck!
+admin-city: Fuck!
+admin-state: Fuck!
+admin-zip: 110
+admin-country: DE
+admin-phone: +86.110
+admin-fax: +86.110
+admin-email: Your@Is.Sb
+
+tech-organization: Fuck!
+tech-name: Fuck! Fuck!
+tech-street: Fuck!
+tech-city: Fuck!
+tech-state: Fuck!
+tech-zip: 110
+tech-country: DE
+tech-phone: +86.110
+tech-fax: +86.110
+tech-email: Your@Is.Sb
+
+billing-organization: Fuck!
+billing-name: Fuck! Fuck!
+billing-street: Fuck!
+billing-city: Fuck!
+billing-state: Fuck!
+billing-zip: 110
+billing-country: DE
+billing-phone: +86.110
+billing-fax: +86.110
+billing-email: Your@Is.Sb
+
+nameserver: ns38.dnsever.com
+nameserver: ns259.dnsever.com
+nameserver: ns91.dnsever.com
+nameserver: ns231.dnsever.com
+nameserver: ns77.dnsever.com
+
+; Be Your Own Services Provider
+; Domains, Backorders, DNS, SSL Certs and much more ...
+
