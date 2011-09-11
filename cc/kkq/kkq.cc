c67c1900e46b331a9555062d96efd3ed3190b79e
xml <xml@sandbox-test1.(none)>
2011-09-10 23:42:31 -0400

    auto checkin

---
+++ cc/kkq/kkq.cc
@@ -0,0 +1,103 @@
+
+Whois Server Version 1.0
+
+Domain names can now be registered with many different competing registrars.
+Go to http://registrar.verisign-grs.com/whois/ for detailed information.
+
+   Domain Name: KKQ.CC
+   Registrar: JIANGSU BANGNING SCIENCE &amp; TECHNOLOGY CO. LTD
+   Whois Server: whois.55hl.com
+   Referral URL: http://www.55hl.com
+   Name Server: DNS1.4CUN.COM
+   Name Server: DNS2.4CUN.COM
+   Status: ACTIVE
+   Updated Date: 08-sep-2011
+   Creation Date: 15-sep-2009
+   Expiration Date: 15-sep-2012
+
+
+&gt;&gt;&gt; Last update of whois database: Sat, 10 Sep 2011 19:23:00 EDT &lt;&lt;&lt;
+
+NOTICE: The expiration date displayed in this record is the date the
+registrar's sponsorship of the domain name registration in the registry is
+currently set to expire. This date does not necessarily reflect the
+expiration date of the domain name registrant's agreement with the
+sponsoring registrar.  Users may consult the sponsoring registrar's
+Whois database to view the registrar's reported date of expiration
+for this registration.
+
+TERMS OF USE: You are not authorized to access or query our Whois
+database through the use of electronic processes that are high-volume and
+automated except as reasonably necessary to register domain names or
+modify existing registrations; the Data in VeriSign's (&quot;VeriSign&quot;) Whois
+database is provided by VeriSign for information purposes only, and to
+assist persons in obtaining information about or related to a domain name
+registration record. VeriSign does not guarantee its accuracy.
+By submitting a Whois query, you agree to abide by the following terms of
+use: You agree that you may use this Data only for lawful purposes and that
+under no circumstances will you use this Data to: (1) allow, enable, or
+otherwise support the transmission of mass unsolicited, commercial
+advertising or solicitations via e-mail, telephone, or facsimile; or
+(2) enable high volume, automated, electronic processes that apply to
+VeriSign (or its computer systems). The compilation, repackaging,
+dissemination or other use of this Data is expressly prohibited without
+the prior written consent of VeriSign. You agree not to use electronic
+processes that are automated and high-volume to access or query the
+Whois database except as reasonably necessary to register domain names
+or modify existing registrations. VeriSign reserves the right to restrict
+your access to the Whois database in its sole discretion to ensure
+operational stability.  VeriSign may restrict or terminate your access to the
+Whois database for failure to abide by these terms of use. VeriSign
+reserves the right to modify these terms at any time.
+
+The Registry database contains ONLY .cc, .tv, and .jobs domains
+and Registrars.
+
+The data in this whois database is provided to you for information
+purposes only, that is, to assist you in obtaining information about or
+related to a domain name registration record. We make this information
+available &quot;as is,&quot; and do not guarantee its accuracy. By submitting a
+whois query, you agree that you will use this data only for lawful
+purposes and that, under no circumstances will you use this data to: (1)
+enable high volume, automated, electronic processes that stress or load
+this whois database system providing you this information; or (2) allow,
+enable, or otherwise support the transmission of mass unsolicited,
+commercial advertising or solicitations via direct mail, electronic
+mail, or by telephone. The compilation, repackaging, dissemination or
+other use of this data is expressly prohibited without prior written
+consent from us. We reserve the right to modify these terms at any time.
+By submitting this query, you agree to abide by these terms.
+
+Domain Name: kkq.cc
+
+Registrant Contact:
+   zhouzhongming
+   zhongming zhou 530148042@qq.com
+   86.09137236454  fax: 86.09137236454
+   shanxipuchengxianyananxilu2hao
+   pucheng Shan2xi 715500
+   CN
+
+Administrative Contact:
+   zhouzhongming
+   zhongming zhou 530148042@qq.com
+   86.09137236454  fax: 86.09137236454
+   shanxipuchengxianyananxilu2hao
+   pucheng Shan2xi 715500
+   CN
+
+Technical Contact:
+   zhouzhongming
+   zhongming zhou 530148042@qq.com
+   86.09137236454  fax: 86.09137236454
+   shanxipuchengxianyananxilu2hao
+   pucheng Shan2xi 715500
+   CN
+
+Billing Contact:
+   zhouzhongming
+   zhongming zhou 530148042@qq.com
+   86.09137236454  fax: 86.09137236454
+   shanxipuchengxianyananxilu2hao
+   pucheng Shan2xi 715500
+   CN
