7f4009338d842fb319d779bb9903d0cad688d6e5
xml <xml@sandbox-test1.(none)>
2011-09-11 00:34:46 -0400

    auto checkin

---
+++ cc/gkp/gkp.cc
@@ -0,0 +1,128 @@
+
+Whois Server Version 1.0
+
+Domain names can now be registered with many different competing registrars.
+Go to http://registrar.verisign-grs.com/whois/ for detailed information.
+
+   Domain Name: GKP.CC
+   Registrar: 1 API GMBH
+   Whois Server: whois.1api.net
+   Referral URL: http://www.1api.net
+   Name Server: F1G1NS1.DNSPOD.NET
+   Name Server: F1G1NS2.DNSPOD.NET
+   Status: ACTIVE
+   Updated Date: 01-aug-2011
+   Creation Date: 31-mar-2008
+   Expiration Date: 31-mar-2012
+
+
+&gt;&gt;&gt; Last update of whois database: Sat, 10 Sep 2011 19:23:00 EDT &lt;&lt;&lt;
+
+NOTICE: The expiration date displayed in this record is the date the
+registrar's sponsorship of the domain name registration in the registry is
+currently set to expire. This date does not necessarily reflect the
+expiration date of the domain name registrant's agreement with the
+sponsoring registrar.  Users may consult the sponsoring registrar's
+Whois database to view the registrar's reported date of expiration
+for this registration.
+
+TERMS OF USE: You are not authorized to access or query our Whois
+database through the use of electronic processes that are high-volume and
+automated except as reasonably necessary to register domain names or
+modify existing registrations; the Data in VeriSign's (&quot;VeriSign&quot;) Whois
+database is provided by VeriSign for information purposes only, and to
+assist persons in obtaining information about or related to a domain name
+registration record. VeriSign does not guarantee its accuracy.
+By submitting a Whois query, you agree to abide by the following terms of
+use: You agree that you may use this Data only for lawful purposes and that
+under no circumstances will you use this Data to: (1) allow, enable, or
+otherwise support the transmission of mass unsolicited, commercial
+advertising or solicitations via e-mail, telephone, or facsimile; or
+(2) enable high volume, automated, electronic processes that apply to
+VeriSign (or its computer systems). The compilation, repackaging,
+dissemination or other use of this Data is expressly prohibited without
+the prior written consent of VeriSign. You agree not to use electronic
+processes that are automated and high-volume to access or query the
+Whois database except as reasonably necessary to register domain names
+or modify existing registrations. VeriSign reserves the right to restrict
+your access to the Whois database in its sole discretion to ensure
+operational stability.  VeriSign may restrict or terminate your access to the
+Whois database for failure to abide by these terms of use. VeriSign
+reserves the right to modify these terms at any time.
+
+The Registry database contains ONLY .cc, .tv, and .jobs domains
+and Registrars.
+; This data is provided by 1api GmbH
+; for information purposes, and to assist persons obtaining information
+; about or related to domain name registration records.
+; 1api GmbH does not guarantee its accuracy.
+; By submitting a WHOIS query, you agree that you will use this data
+; only for lawful purposes and that, under no circumstances, you will
+; use this data to
+; 1) allow, enable, or otherwise support the transmission of mass
+;    unsolicited, commercial advertising or solicitations via E-mail
+;    (spam); or
+; 2) enable high volume, automated, electronic processes that apply
+;    to this WHOIS server.
+; These terms may be changed without prior notice.
+; By submitting this query, you agree to abide by this policy.
+
+
+
+DOMAIN: GKP.CC
+
+RSP: 1api GmbH
+URL: http://www.1api.net/
+
+created-date: 2008-03-31 15:27:16
+updated-date: 2011-08-01 14:46:27
+registration-expiration-date: 2012-03-31 15:27:16
+
+owner-organization: Beijing SJLR
+owner-name: Nan Chen
+owner-street: 2801 Tower D, Elite Apartment Tongzhou Dist.
+owner-city: Beijing
+owner-state: Beijing
+owner-zip: 100121
+owner-country: CN
+owner-phone: +86.13901004443
+owner-fax:
+owner-email: gokeeper@gmail.com
+
+admin-organization: Beijing SJLR
+admin-name: Nan Chen
+admin-street: 2801 Tower D, Elite Apartment Tongzhou Dist.
+admin-city: Beijing
+admin-state: Beijing
+admin-zip: 100121
+admin-country: CN
+admin-phone: +86.13901004443
+admin-fax:
+admin-email: gokeeper@gmail.com
+
+tech-organization: Beijing SJLR
+tech-name: Nan Chen
+tech-street: 2801 Tower D, Elite Apartment Tongzhou Dist.
+tech-city: Beijing
+tech-state: Beijing
+tech-zip: 100121
+tech-country: CN
+tech-phone: +86.13901004443
+tech-fax:
+tech-email: gokeeper@gmail.com
+
+billing-organization: Beijing SJLR
+billing-name: Nan Chen
+billing-street: 2801 Tower D, Elite Apartment Tongzhou Dist.
+billing-city: Beijing
+billing-state: Beijing
+billing-zip: 100121
+billing-country: CN
+billing-phone: +86.13901004443
+billing-fax:
+billing-email: gokeeper@gmail.com
+
+nameserver: f1g1ns1.dnspod.net
+nameserver: f1g1ns2.dnspod.net
+
+
---
+++ ru/marusya-restaurant/marusya-restaurant.ru
@@ -0,0 +1,18 @@
+% By submitting a query to RIPN's Whois Service
+% you agree to abide by the following terms of use:
+% http://www.ripn.net/about/servpol.html#3.2 (in Russian)
+% http://www.ripn.net/about/en/servpol.html#3.2 (in English).
+
+domain:     MARUSYA-RESTAURANT.RU
+nserver:    ns1.agava.net.ru.
+nserver:    ns2.agava.net.ru.
+state:      REGISTERED, DELEGATED, VERIFIED
+person:     Private Person
+e-mail:     7361515@gmail.com
+registrar:  AGAVA-REG-RIPN
+created:    2009.12.21
+paid-till:  2011.12.21
+source:     TCI
+
+Last updated on 2011.09.11 08:33:42 MSK/MSD
+
