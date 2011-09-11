fa9109d4fd5d05070e5607756aaeed419fbd887a
xml <xml@sandbox-test1.(none)>
2011-09-11 15:38:44 -0400

    auto checkin

--- com/sxmqzs/sxmqzs.com
+++ com/sxmqzs/sxmqzs.com
@@ -16,7 +16,7 @@ for detailed information.
    Creation Date: 04-may-2010
    Expiration Date: 04-may-2012
 
-&gt;&gt;&gt; Last update of whois database: Sat, 10 Sep 2011 18:52:36 UTC &lt;&lt;&lt;
+&gt;&gt;&gt; Last update of whois database: Sun, 11 Sep 2011 19:41:39 UTC &lt;&lt;&lt;
 
 NOTICE: The expiration date displayed in this record is the date the
 registrar's sponsorship of the domain name registration in the registry is
---
+++ pl/1and1/1and1.pl
@@ -0,0 +1,35 @@
+
+DOMAIN NAME:           1and1.pl
+registrant type:       organization
+nameservers:           ns-1and1.ui-dns.org.
+                       ns-1and1.ui-dns.com.
+                       ns-1and1.ui-dns.biz.
+                       ns-1and1.ui-dns.de.
+created:               2005.05.19 10:27:10
+last modified:         2011.06.27 14:22:30
+
+no option
+
+TECHNICAL CONTACT:
+company: Host Master
+        1&amp;1 Internet Sp. z o.o.
+street: ul. ZlOTA, nr 59
+city: 00-120 Warszawa
+location: PL
+phone:  +22.3974316
+fax:  +22.3974317
+last modified: 2011.06.27
+
+
+REGISTRAR:
+1&amp;1 Internet AG
+Brauerstrasse 48
+76135 Karlsruhe
+Niemcy/Germany
+tel:+48.223974316
+faks:+48.223974317
+pomoc@1and1.pl
+
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
