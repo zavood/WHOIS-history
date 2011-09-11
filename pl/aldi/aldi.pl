ac616727750058304152350df6f7e06f5d81fcc1
xml <xml@sandbox-test1.(none)>
2011-09-11 03:29:41 -0400

    auto checkin

---
+++ pl/aldi/aldi.pl
@@ -0,0 +1,31 @@
+
+DOMAIN NAME:           aldi.pl
+registrant type:       organization
+nameservers:           dns017.arvato-systems.de.
+                       dns009.arvato-systems.de.
+created:               2006.08.17 13:42:46
+last modified:         2010.10.13 14:11:21
+
+no option
+
+TECHNICAL CONTACT:
+company: Hostmaster nmi
+        arvato systems GmbH
+street: An der Autobahn
+city: 33311 Guetersloh
+location: DE
+phone:  +49.5241808000
+fax:  +49.52418068000
+last modified: 2008.12.15
+
+
+REGISTRAR:
+1API GmbH
+Talstrasse 27
+66424 Homburg
+Niemcy/ Germany
+tel.: +49.68416984200
+email: info@1api.net
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
--- ru/veneva/veneva.ru
+++ ru/veneva/veneva.ru
@@ -3,15 +3,17 @@
 % http://www.ripn.net/about/servpol.html#3.2 (in Russian)
 % http://www.ripn.net/about/en/servpol.html#3.2 (in English).
 
-domain:     VDYAKONOV.RU
+domain:     VENEVA.RU
 nserver:    ns1.agava.net.ru.
 nserver:    ns2.agava.net.ru.
-state:      REGISTERED, DELEGATED, VERIFIED
-person:     Private Person
-e-mail:     bikbaeva@yandex.ru
+state:      REGISTERED, DELEGATED, UNVERIFIED
+org:        PBOYuL Makhyel Dyenis Alyeksandrovich
+phone:      +7 08754 53380
+fax-no:     +7 08754 53380
+e-mail:     maxell@kireev.tula.net
 registrar:  R01-REG-RIPN
-created:    2009.08.26
-paid-till:  2012.08.26
+created:    2005.08.25
+paid-till:  2012.08.25
 source:     TCI
 
 Last updated on 2011.09.11 11:28:42 MSK/MSD
