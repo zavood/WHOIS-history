70ad49db8271292263b3ae6484561a22b7881f32
xml <xml@sandbox-test1.(none)>
2011-09-11 13:41:53 -0400

    auto checkin

---
+++ cc/qqwg/qqwg.cc
@@ -0,0 +1,131 @@
+
+Whois Server Version 1.0
+
+Domain names can now be registered with many different competing registrars.
+Go to http://registrar.verisign-grs.com/whois/ for detailed information.
+
+   Domain Name: QQWG.CC
+   Registrar: 1 API GMBH
+   Whois Server: whois.1api.net
+   Referral URL: http://www.1api.net
+   Name Server: NS1.HOST70.COM
+   Name Server: NS2.HOST70.COM
+   Status: CLIENT-XFER-PROHIBITED
+   Updated Date: 14-aug-2011
+   Creation Date: 05-jun-2011
+   Expiration Date: 05-jun-2012
+
+
+&gt;&gt;&gt; Last update of whois database: Sun, 11 Sep 2011 13:22:48 EDT &lt;&lt;&lt;
+
+NOTICE: The expiration date displayed in this record is the date the
+registrar's sponsorship of the domain name registration in the registry is
+currently set to expire. This date does not necessarily reflect the
+expiration date of the domain name registrant's agreement with the
+sponsoring registrar.  Users may consult the sponsoring registrar's
+Whois database to view the registrar's reported date of expiration
+for this registration.
+
+TERMS OF USE: You are not authorized to access or query our Whois
+database through the use of electronic processes that are high-volume and
+automated except as reasonably necessary to register domain names or
+modify existing registrations; the Data in VeriSign's (&quot;VeriSign&quot;) Whois
+database is provided by VeriSign for information purposes only, and to
+assist persons in obtaining information about or related to a domain name
+registration record. VeriSign does not guarantee its accuracy.
+By submitting a Whois query, you agree to abide by the following terms of
+use: You agree that you may use this Data only for lawful purposes and that
+under no circumstances will you use this Data to: (1) allow, enable, or
+otherwise support the transmission of mass unsolicited, commercial
+advertising or solicitations via e-mail, telephone, or facsimile; or
+(2) enable high volume, automated, electronic processes that apply to
+VeriSign (or its computer systems). The compilation, repackaging,
+dissemination or other use of this Data is expressly prohibited without
+the prior written consent of VeriSign. You agree not to use electronic
+processes that are automated and high-volume to access or query the
+Whois database except as reasonably necessary to register domain names
+or modify existing registrations. VeriSign reserves the right to restrict
+your access to the Whois database in its sole discretion to ensure
+operational stability.  VeriSign may restrict or terminate your access to the
+Whois database for failure to abide by these terms of use. VeriSign
+reserves the right to modify these terms at any time.
+
+The Registry database contains ONLY .cc, .tv, and .jobs domains
+and Registrars.
+; This data is provided by HEXONET Services Inc.
+; for information purposes, and to assist persons obtaining information
+; about or related to domain name registration records.
+; HEXONET Services Inc. does not guarantee its accuracy.
+; By submitting a WHOIS query, you agree that you will use this data
+; only for lawful purposes and that, under no circumstances, you will
+; use this data to
+; 1) allow, enable, or otherwise support the transmission of mass
+;    unsolicited, commercial advertising or solicitations via E-mail
+;    (spam); or
+; 2) enable high volume, automated, electronic processes that apply
+;    to this WHOIS server.
+; These terms may be changed without prior notice.
+; By submitting this query, you agree to abide by this policy.
+
+
+
+DOMAIN: QQWG.CC
+
+RSP: HEXONET Services Inc.
+URL: http://www.HEXONET.net
+
+created-date: 2011-06-05 11:56:39
+updated-date: 2011-08-14 15:09:54
+registration-expiration-date: 2012-06-05 11:56:40
+
+owner-organization: Zhan Quanyuan
+owner-name: zhan quanyuan
+owner-street: PoYang YouDun Huxiacun 080
+owner-city: ShangRao
+owner-state: Jiangxi
+owner-zip: 333119
+owner-country: CN
+owner-phone: +86.79383456985
+owner-fax: +86.79383456985
+owner-email: 415693934@qq.com
+
+admin-organization: Zhan Quanyuan
+admin-name: zhan quanyuan
+admin-street: PoYang YouDun Huxiacun 080
+admin-city: ShangRao
+admin-state: Jiangxi
+admin-zip: 333119
+admin-country: CN
+admin-phone: +86.79383456985
+admin-fax: +86.79383456985
+admin-email: 415693934@qq.com
+
+tech-organization: Zhan Quanyuan
+tech-name: zhan quanyuan
+tech-street: PoYang YouDun Huxiacun 080
+tech-city: ShangRao
+tech-state: Jiangxi
+tech-zip: 333119
+tech-country: CN
+tech-phone: +86.79383456985
+tech-fax: +86.79383456985
+tech-email: 415693934@qq.com
+
+billing-organization: Zhan Quanyuan
+billing-name: zhan quanyuan
+billing-street: PoYang YouDun Huxiacun 080
+billing-city: ShangRao
+billing-state: Jiangxi
+billing-zip: 333119
+billing-country: CN
+billing-phone: +86.79383456985
+billing-fax: +86.79383456985
+billing-email: 415693934@qq.com
+
+nameserver: ns1.host70.com
+nameserver: ns2.host70.com
+
+; --
+; Be Your Own Services Provider
+; Domains, Backorders, DNS, SSL Certs and much more ...
+
