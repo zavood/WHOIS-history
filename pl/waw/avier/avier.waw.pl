df20cdda4510df4ce9b7a15c3245b9408e44191b
xml <xml@sandbox-test1.(none)>
2011-09-11 19:30:33 -0400

    auto checkin

---
+++ pl/waw/avier/avier.waw.pl
@@ -0,0 +1,30 @@
+
+DOMAIN NAME:           avier.waw.pl
+registrant type:       individual
+nameservers:           sdns1.ovh.net.
+                       ns311000.ovh.net.
+created:               2009.10.04 12:38:35
+last modified:         2010.10.01 21:23:24
+
+no option
+
+TECHNICAL CONTACT:
+company: MasterNET.pl Szczepanik Adrian
+street: ul. Wybickiego 16/6
+city: 31-261 Krakow
+location: PL
+phone:  +48.123502001
+fax:  +48.123783951
+last modified: 2011.06.24
+
+
+REGISTRAR:
+Consulting Service Robert Siebielski
+ul. Domaniewska 35A lok.1B
+02-672 Warszawa
+Polska/Poland
++48.22 8538888
+domeny@ConsultingService.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
