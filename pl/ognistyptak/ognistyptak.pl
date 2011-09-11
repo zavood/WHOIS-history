ccc9cdb6afac5a322fdddc9d0757a5e31fee4f16
xml <xml@sandbox-test1.(none)>
2011-09-11 12:25:20 -0400

    auto checkin

---
+++ pl/ognistyptak/ognistyptak.pl
@@ -0,0 +1,21 @@
+
+DOMAIN NAME:           ognistyptak.pl
+registrant type:       organization
+nameservers:           dns2.zillion.pl. [213.241.6.249]
+                       dns.zillion.pl. [213.241.6.248]
+created:               2004.01.14 06:45:08
+last modified:         2005.06.23 06:30:50
+
+no option
+
+
+REGISTRAR:
+NASK
+ul. Wawozowa 18
+02-796 Warszawa
+Polska/Poland
++48.22 3808300
+info@dns.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
