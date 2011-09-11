2d2992b9692469d06985e2247b0cc91921099489
xml <xml@sandbox-test1.(none)>
2011-09-11 01:06:56 -0400

    auto checkin

---
+++ rs/jub/jub.rs
@@ -0,0 +1,42 @@
+%
+%This is the RNIDS Whois server.
+%
+% Date Format         : DD.MM.YYYY
+% Whois Server Version: 1.0.0
+%
+% Rights restricted by copyright.
+% See http://www.rnids.rs/whois_en
+%
+%
+%
+% Ovo je odgovor od RNIDS Whois servera.
+%
+% Format datuma     : DD.MM.YYYY
+% Verzija Whois Servera : 1.0.0
+%
+% Sva prava zadr&Aring;&frac34;ana. Za vi&Aring;&iexcl;e informacija.
+% pogledajte http://www.rnids.rs/whois_sr
+
+Domain name: jub.rs.
+Domain status: Active
+Registration date: 06.05.2008 13:01:39
+Modification date: 27.04.2011 13:47:59
+Expiration date: 06.05.2012 13:01:39
+Registrar: BeotelNet-ISP d.o.o.
+
+
+Owner: JUB d.o.o.
+Address: Dositejeva 32, Beograd, Srbija
+ID Number: 100427800
+Tax ID: 100427800
+
+
+DNS: ns1.needhost.com. - 195.246.14.14
+DNS: ns2.needhost.com. - 213.250.2.101
+
+
+Administrative contact: Dejan Kosti&Auml;‡, JUB d.o.o.
+Address: Novo naselje 32, &Aring;&nbsp;imanovci, Beograd, Srbija
+
+Technical contact: Dejan Kosti&Auml;‡, JUB d.o.o.
+Address: Novo naselje 32, &Aring;&nbsp;imanovci, Beograd, Srbija
