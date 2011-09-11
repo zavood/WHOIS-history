c3efe8e916c7a8a366ce9e4e5ad6da718a44d88c
xml <xml@sandbox-test1.(none)>
2011-09-11 00:22:43 -0400

    auto checkin

---
+++ pl/avx/avx.pl
@@ -0,0 +1,29 @@
+
+DOMAIN NAME:           avx.pl
+registrant type:       organization
+nameservers:           sec.avx.pl. [217.149.240.52]
+                       dns2.artnet.com.pl. [91.200.32.7]
+                       ns1.avx.pl. [217.149.240.46]
+created:               2004.07.25 01:29:56
+last modified:         2011.07.21 10:55:07
+
+no option
+
+TECHNICAL CONTACT:
+company: Artnet Sp. z o.o. (AVX Hosting)
+street: Waly Piastowskie 1
+city: 80-855 Gdansk
+location: PL
+last modified: 2006.11.10
+
+
+REGISTRAR:
+Artnet sp. z o.o.
+ul. Waly Piastowskie 1
+80-855 Gdansk
+Polska/Poland
++48.58 3074289
+info@artnet.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
