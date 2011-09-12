6d4a247b72dc7287d04c76e21300d1a4b3db426f
xml <xml@sandbox-test1.(none)>
2011-09-12 10:08:11 -0400

    auto checkin

--- cc/co/co.cc
+++ cc/co/co.cc
@@ -19,7 +19,7 @@ Go to http://registrar.verisign-grs.com/whois/ for detailed information.
    Expiration Date: 18-aug-2020
 
 
-&gt;&gt;&gt; Last update of whois database: Sat, 10 Sep 2011 19:23:00 EDT &lt;&lt;&lt;
+&gt;&gt;&gt; Last update of whois database: Mon, 12 Sep 2011 07:23:05 EDT &lt;&lt;&lt;
 
 NOTICE: The expiration date displayed in this record is the date the
 registrar's sponsorship of the domain name registration in the registry is
