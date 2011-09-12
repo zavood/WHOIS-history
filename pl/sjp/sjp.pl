ac8c846c7da60dda55914822b946006485dbbaa3
xml <xml@sandbox-test1.(none)>
2011-09-12 19:23:43 -0400

    auto checkin

---
+++ pl/sjp/sjp.pl
@@ -0,0 +1,24 @@
+
+DOMAIN NAME:           sjp.pl
+registrant type:       organization
+nameservers:           ns3.netart.pl. [85.128.130.10]
+                       ns1.netart.pl. [85.128.128.10]
+                       ns2.netart.pl. [85.128.129.10]
+created:               2006.02.11 14:51:06
+last modified:         2011.05.15 11:12:41
+
+no option
+
+
+REGISTRAR:
+NetArt Spolka Akcyjna S.K.A.
+ul. Cystersow 20A
+31-553 Krakow
+Polska/Poland
++48.801 33 22 33
++48.12 297 88 10
++48.12 297 88 08
+biuro@nazwa.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
