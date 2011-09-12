877db13676e97da8c1ac4568472cab5b3148c719
xml <xml@sandbox-test1.(none)>
2011-09-12 16:05:14 -0400

    auto checkin

---
+++ pl/tim/tim.pl
@@ -0,0 +1,22 @@
+
+DOMAIN NAME:           tim.pl
+registrant type:       organization
+nameservers:           dns.tim.pl. [212.160.143.227]
+                       ns.tpnet.pl. [80.50.50.50]
+created:               1998.09.24 13:00:00
+last modified:         2004.09.28 06:31:06
+
+option created:        2011.06.05 21:16:51
+option expiration date:       2014.06.05 21:16:51
+
+
+REGISTRAR:
+NASK
+ul. Wawozowa 18
+02-796 Warszawa
+Polska/Poland
++48.22 3808300
+info@dns.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
