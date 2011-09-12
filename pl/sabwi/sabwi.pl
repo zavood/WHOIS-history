9301024b85ad2f0faafef6c487d6c3b255cd02d3
xml <xml@sandbox-test1.(none)>
2011-09-12 14:39:24 -0400

    auto checkin

---
+++ pl/sabwi/sabwi.pl
@@ -0,0 +1,22 @@
+
+DOMAIN NAME:           sabwi.pl
+registrant type:       individual
+nameservers:           ns1.dla.pl. [91.121.223.184]
+                       ns2.dla.pl. [178.33.50.22]
+created:               2010.12.10 08:26:04
+last modified:         2011.09.10 15:59:43
+
+no option
+TECHNICAL CONTACT: data restricted
+
+
+REGISTRAR:
+AZ.pl Sp. z o.o.
+ul. Niedzialkowskiego 24(IX p.)
+71-410 Szczecin
+Polska/Poland
++48.801080031
+domena@az.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
