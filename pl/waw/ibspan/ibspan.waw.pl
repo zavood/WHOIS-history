3ef79d4b0ff5b639361c06d75dfe3e31e5e81442
xml <xml@sandbox-test1.(none)>
2011-09-12 17:12:02 -0400

    auto checkin

---
+++ pl/waw/ibspan/ibspan.waw.pl
@@ -0,0 +1,23 @@
+
+DOMAIN NAME:           ibspan.waw.pl
+registrant type:       organization
+nameservers:           see-you-later.wsisiz.edu.pl. [213.135.44.40]
+                       ibs5.ibspan.waw.pl. [213.135.34.25]
+                       ibs4.ibspan.waw.pl. [213.135.34.24]
+                       ibs2.ibspan.waw.pl. [213.135.34.22]
+created:               1995.01.01 12:00:00
+last modified:         2005.08.17 13:36:14
+
+no option
+
+
+REGISTRAR:
+NASK
+ul. Wawozowa 18
+02-796 Warszawa
+Polska/Poland
++48.22 3808300
+info@dns.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
