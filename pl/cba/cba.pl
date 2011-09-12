af1cf0bb008a96e5a9e9472a0fb8c7821684590c
xml <xml@sandbox-test1.(none)>
2011-09-12 07:49:25 -0400

    auto checkin

---
+++ pl/cba/cba.pl
@@ -0,0 +1,22 @@
+
+DOMAIN NAME:           cba.pl
+registrant type:       individual
+nameservers:           ns1.cba.pl. [85.17.25.217]
+                       ns2.cba.pl. [85.17.25.217]
+created:               2005.01.14 14:36:58
+last modified:         2010.12.27 15:33:42
+
+option created:        2011.02.08 17:05:36
+option expiration date:       2014.02.08 17:05:36
+
+
+REGISTRAR:
+Consulting Service Robert Siebielski
+ul. Domaniewska 35A lok.1B
+02-672 Warszawa
+Polska/Poland
++48.22 8538888
+domeny@ConsultingService.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
