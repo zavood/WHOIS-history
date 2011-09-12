f4ff9471bf3cdd9528ecb7e0ad4f3d5154ba114a
xml <xml@sandbox-test1.(none)>
2011-09-11 21:09:23 -0400

    auto checkin

---
+++ pl/gemius/gemius.pl
@@ -0,0 +1,25 @@
+
+DOMAIN NAME:           gemius.pl
+registrant type:       organization
+nameservers:           ns2.gemius.pl. [62.168.111.101]
+                       ns1.gemius.pl. [213.189.48.235]
+                       ns3.gemius.pl. [77.78.99.17]
+created:               2000.05.16 13:00:00
+last modified:         2011.05.24 11:41:12
+
+option created:        2011.05.20 03:40:22
+option expiration date:       2014.05.20 03:40:22
+
+
+REGISTRAR:
+NetArt Spolka Akcyjna S.K.A.
+ul. Cystersow 20A
+31-553 Krakow
+Polska/Poland
++48.801 33 22 33
++48.12 297 88 10
++48.12 297 88 08
+biuro@nazwa.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
