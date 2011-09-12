d3f592c3a46f82131834e4b40d42ab0d24d2e839
xml <xml@sandbox-test1.(none)>
2011-09-12 06:52:48 -0400

    auto checkin

---
+++ pl/gawex/gawex.pl
@@ -0,0 +1,32 @@
+
+DOMAIN NAME:           gawex.pl
+registrant type:       organization
+nameservers:           ns1.interian.pl. [178.63.136.167]
+                       dns.gawex.pl. [84.205.31.2]
+                       ns1.gawex.pl. [89.239.127.9]
+created:               2003.01.01 12:00:00
+last modified:         2010.10.05 09:13:15
+
+no option
+
+TECHNICAL CONTACT:
+company: Albert Jerka, Andrzej Kostrzewa
+        AZ.pl Sp. z o.o.
+street: Sosnowa 6a
+city: 71-468 Szczecin
+location: PL
+phone:  +48.914243780
+fax:  +48.914243799
+last modified: 2010.07.21
+
+
+REGISTRAR:
+AZ.pl Sp. z o.o.
+ul. Niedzialkowskiego 24(IX p.)
+71-410 Szczecin
+Polska/Poland
++48.801080031
+domena@az.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
