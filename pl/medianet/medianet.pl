bfb8db54fbfcd3b27536744844c9dad3a52305cc
xml <xml@sandbox-test1.(none)>
2011-09-12 19:21:42 -0400

    auto checkin

---
+++ pl/medianet/medianet.pl
@@ -0,0 +1,24 @@
+
+DOMAIN NAME:           medianet.pl
+registrant type:       organization
+nameservers:           dns1.medianet.pl. [193.192.161.134]
+                       dns2.medianet.pl. [193.192.161.141]
+created:               1996.10.14 13:00:00
+last modified:         2010.12.22 14:55:18
+
+option created:        2009.10.10 15:21:32
+option expiration date:       2012.10.10 15:21:32
+
+
+REGISTRAR:
+NetArt Spolka Akcyjna S.K.A.
+ul. Cystersow 20A
+31-553 Krakow
+Polska/Poland
++48.801 33 22 33
++48.12 297 88 10
++48.12 297 88 08
+biuro@nazwa.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
