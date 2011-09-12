e472f0425bdd5e8a29a47c480d3847820f75354d
xml <xml@sandbox-test1.(none)>
2011-09-12 12:31:05 -0400

    auto checkin

---
+++ pl/waw/oeiizk/oeiizk.waw.pl
@@ -0,0 +1,31 @@
+
+DOMAIN NAME:           oeiizk.waw.pl
+registrant type:       organization
+nameservers:           see-you-later.wsisiz.edu.pl. [213.135.44.40]
+                       dns.oeiizk.waw.pl. [212.244.131.3]
+created:               1998.07.08 13:00:00
+last modified:         2011.03.09 16:33:42
+
+no option
+
+TECHNICAL CONTACT:
+company: ACTIVE 24 sp. z o.o.
+street: Al. Niepodleglosci 69
+city: 02-626 Warszawa
+location: PL
+phone:  +48.801889985
+fax:  +48.223227648
+last modified: 2010.09.07
+
+
+REGISTRAR:
+Active 24 sp. z o.o.
+Al. Niepodleglosci 69
+02-626 Warszawa
+Polska/Poland
++48.801889985
++48.22 3227733
+bok@active24.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
