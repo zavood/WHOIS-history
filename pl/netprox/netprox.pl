a6acdabbb47a78ddaa212dbf674d2b130faffd26
xml <xml@sandbox-test1.(none)>
2011-09-13 18:32:32 -0400

    auto checkin

---
+++ pl/netprox/netprox.pl
@@ -0,0 +1,21 @@
+
+DOMAIN NAME:           netprox.pl
+registrant type:       organization
+nameservers:           ns2.amm-komputer.pl. [195.149.231.66]
+                       ns1.amm-komputer.pl. [195.149.230.253]
+created:               2006.10.06 14:19:55
+last modified:         2010.11.08 11:25:16
+
+no option
+
+
+REGISTRAR:
+Domainmaker sp. z o.o.
+ul. Gdanska 107
+85-022 Bydgoszcz
+Polska/Poland
++48.525524220
+www.domainmaker.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
