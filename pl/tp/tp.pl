f27c5b7b0da35f32104e03b2d91b8aa37fb28f2b
xml <xml@sandbox-test1.(none)>
2011-09-14 12:30:12 -0400

    auto checkin

---
+++ pl/tp/tp.pl
@@ -0,0 +1,33 @@
+
+DOMAIN NAME:           tp.pl
+registrant type:       organization
+nameservers:           ns2.tpnet.pl. [80.50.50.150]
+                       ns3.tpnet.pl. [80.50.50.200]
+                       ns1.tpnet.pl. [80.50.50.100]
+created:               1998.11.03 12:00:00
+last modified:         2010.11.26 17:04:56
+
+option created:        2011.01.27 17:08:30
+option expiration date:       2014.01.27 17:08:30
+
+TECHNICAL CONTACT:
+company: Albert Jerka, Andrzej Kostrzewa
+        AZ.pl Sp. z o.o.
+street: Sosnowa 6a
+city: 71-468 Szczecin
+location: PL
+phone:  +48.914243780
+fax:  +48.914243799
+last modified: 2010.11.26
+
+
+REGISTRAR:
+AZ.pl Sp. z o.o.
+ul. Niedzialkowskiego 24(IX p.)
+71-410 Szczecin
+Polska/Poland
++48.801080031
+domena@az.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
