d873a031ccc01df7d08cd9b155673ec76a555905
xml <xml@sandbox-test1.(none)>
2011-09-14 13:37:37 -0400

    auto checkin

---
+++ pl/adler-auto/adler-auto.pl
@@ -0,0 +1,21 @@
+
+DOMAIN NAME:           adler-auto.pl
+registrant type:       organization
+nameservers:           millenium.terragraph.pl. [212.106.184.4]
+                       dns2000.terragraph.pl. [79.139.6.36]
+created:               2002.11.22 12:00:00
+last modified:         2010.11.23 11:48:05
+
+no option
+
+
+REGISTRAR:
+Domainmaker sp. z o.o.
+ul. Gdanska 107
+85-022 Bydgoszcz
+Polska/Poland
++48.525524220
+www.domainmaker.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
