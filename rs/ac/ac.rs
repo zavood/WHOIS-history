d6006c71931e42d5a66dd99670dd3934ebd3f189
xml <xml@sandbox-test1.(none)>
2011-09-14 03:32:49 -0400

    auto checkin

---
+++ rs/ac/ac.rs
@@ -0,0 +1,47 @@
+%
+%This is the RNIDS Whois server.
+%
+% Date Format         : DD.MM.YYYY
+% Whois Server Version: 1.0.0
+%
+% Rights restricted by copyright.
+% See http://www.rnids.rs/whois_en
+%
+%
+%
+% Ovo je odgovor od RNIDS Whois servera.
+%
+% Format datuma     : DD.MM.YYYY
+% Verzija Whois Servera : 1.0.0
+%
+% Sva prava zadr&Aring;&frac34;ana. Za vi&Aring;&iexcl;e informacija.
+% pogledajte http://www.rnids.rs/whois_sr
+
+Domain name: ac.rs.
+Domain status: Active
+Registration date: 10.03.2008 12:00:00
+Modification date: 03.11.2008 23:31:49
+Expiration date: 10.03.2108 12:00:00
+Registrar: RNIDS
+
+
+Owner: RNIDS
+Address: &Aring;&frac12;or&Aring;&frac34;a Klemansoa 18a, Beograd, Srbija
+ID Number: 17680544
+Tax ID: 104852190
+
+
+DNS: odisej.telekom.rs. - 195.178.32.2
+DNS: ns.rcub.bg.ac.rs. - 147.91.1.5
+DNS: gaea.rcub.bg.ac.rs. - 147.91.1.7
+DNS: ns1.uns.ac.rs. - 147.91.173.4
+DNS: ban.junis.ni.ac.rs. - 160.99.1.1
+DNS: ns.unic.kg.ac.rs. - 147.91.209.2
+DNS: ns.etf.bg.ac.rs. - 147.91.8.6
+DNS: ns2.iif.hu. - 193.225.12.59
+
+Administrative contact: Zoran Jovanovi&Auml;á, RCUB - Ra&Auml;çunski centar Univerziteta u Beogradu
+Address: Kumanovska bb, Beograd, Srbija
+
+Technical contact: Mara Bukvi&Auml;á, RCUB - Ra&Auml;çunski centar Univerziteta u Beogradu
+Address: Kumanovska bb, Beograd, Srbija
