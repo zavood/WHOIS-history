0db50dfeb49a689417d8a62be4735159b7605333
xml <xml@sandbox-test1.(none)>
2011-09-14 10:42:04 -0400

    auto checkin

---
+++ cc/bafa/bafa.cc
@@ -0,0 +1,131 @@
+
+Whois Server Version 1.0
+
+Domain names can now be registered with many different competing registrars.
+Go to http://registrar.verisign-grs.com/whois/ for detailed information.
+
+   Domain Name: BAFA.CC
+   Registrar: 1 API GMBH
+   Whois Server: whois.1api.net
+   Referral URL: http://www.1api.net
+   Name Server: NS1.EW100.COM
+   Name Server: NS2.EW100.COM
+   Status: CLIENT-XFER-PROHIBITED
+   Updated Date: 12-jun-2011
+   Creation Date: 09-dec-2010
+   Expiration Date: 09-dec-2011
+
+
+&gt;&gt;&gt; Last update of whois database: Wed, 14 Sep 2011 07:23:06 EDT &lt;&lt;&lt;
+
+NOTICE: The expiration date displayed in this record is the date the
+registrar's sponsorship of the domain name registration in the registry is
+currently set to expire. This date does not necessarily reflect the
+expiration date of the domain name registrant's agreement with the
+sponsoring registrar.  Users may consult the sponsoring registrar's
+Whois database to view the registrar's reported date of expiration
+for this registration.
+
+TERMS OF USE: You are not authorized to access or query our Whois
+database through the use of electronic processes that are high-volume and
+automated except as reasonably necessary to register domain names or
+modify existing registrations; the Data in VeriSign's (&quot;VeriSign&quot;) Whois
+database is provided by VeriSign for information purposes only, and to
+assist persons in obtaining information about or related to a domain name
+registration record. VeriSign does not guarantee its accuracy.
+By submitting a Whois query, you agree to abide by the following terms of
+use: You agree that you may use this Data only for lawful purposes and that
+under no circumstances will you use this Data to: (1) allow, enable, or
+otherwise support the transmission of mass unsolicited, commercial
+advertising or solicitations via e-mail, telephone, or facsimile; or
+(2) enable high volume, automated, electronic processes that apply to
+VeriSign (or its computer systems). The compilation, repackaging,
+dissemination or other use of this Data is expressly prohibited without
+the prior written consent of VeriSign. You agree not to use electronic
+processes that are automated and high-volume to access or query the
+Whois database except as reasonably necessary to register domain names
+or modify existing registrations. VeriSign reserves the right to restrict
+your access to the Whois database in its sole discretion to ensure
+operational stability.  VeriSign may restrict or terminate your access to the
+Whois database for failure to abide by these terms of use. VeriSign
+reserves the right to modify these terms at any time.
+
+The Registry database contains ONLY .cc, .tv, and .jobs domains
+and Registrars.
+; This data is provided by EW100.com
+; for information purposes, and to assist persons obtaining information
+; about or related to domain name registration records.
+; EW100.com does not guarantee its accuracy.
+; By submitting a WHOIS query, you agree that you will use this data
+; only for lawful purposes and that, under no circumstances, you will
+; use this data to
+; 1) allow, enable, or otherwise support the transmission of mass
+;    unsolicited, commercial advertising or solicitations via E-mail
+;    (spam); or
+; 2) enable high volume, automated, electronic processes that apply
+;    to this WHOIS server.
+; These terms may be changed without prior notice.
+; By submitting this query, you agree to abide by this policy.
+
+
+
+DOMAIN: BAFA.CC
+
+RSP: EW100.com
+URL: http://www.EW100.com
+
+created-date: 2010-12-10 02:14:18
+updated-date: 2011-06-13 03:38:27
+registration-expiration-date: 2011-12-10 02:14:18
+
+owner-organization: wu dongsheng
+owner-name: wu dongsheng
+owner-street: zhongheng4qu
+owner-city: leshan
+owner-state: Sichuan
+owner-zip: 100571
+owner-country: CN
+owner-phone: +86.41225959
+owner-fax: +86.13122489537
+owner-email: xx1147@163.com
+
+admin-organization: wu dongsheng
+admin-name: wu dongsheng
+admin-street: zhongheng4qu
+admin-city: leshan
+admin-state: Sichuan
+admin-zip: 100571
+admin-country: CN
+admin-phone: +86.41225959
+admin-fax: +86.13122489537
+admin-email: xx1147@163.com
+
+tech-organization: wu dongsheng
+tech-name: wu dongsheng
+tech-street: zhongheng4qu
+tech-city: leshan
+tech-state: Sichuan
+tech-zip: 100571
+tech-country: CN
+tech-phone: +86.41225959
+tech-fax: +86.13122489537
+tech-email: xx1147@163.com
+
+billing-organization: wu dongsheng
+billing-name: wu dongsheng
+billing-street: zhongheng4qu
+billing-city: leshan
+billing-state: Sichuan
+billing-zip: 100571
+billing-country: CN
+billing-phone: +86.41225959
+billing-fax: +86.13122489537
+billing-email: xx1147@163.com
+
+nameserver: ns1.ew100.com
+nameserver: ns2.ew100.com
+
+; --
+; Be Your Own Services Provider
+; Domains, Backorders, DNS, SSL Certs and much more ...
+
