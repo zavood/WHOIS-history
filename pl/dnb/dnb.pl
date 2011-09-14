9e353f05d7d16370c86c381f9ce0cde5f285fdc8
xml <xml@sandbox-test1.(none)>
2011-09-13 20:11:31 -0400

    auto checkin

---
+++ pl/dnb/dnb.pl
@@ -0,0 +1,22 @@
+
+DOMAIN NAME:           dnb.pl
+registrant type:       individual
+nameservers:           ns1.plock.biz.
+                       ns2.plock.biz.
+created:               2005.10.13 21:51:12
+last modified:         2010.10.12 14:19:25
+
+option created:        2009.10.12 14:30:35
+option expiration date:       2012.10.12 14:30:35
+
+
+REGISTRAR:
+Consulting Service Robert Siebielski
+ul. Domaniewska 35A lok.1B
+02-672 Warszawa
+Polska/Poland
++48.22 8538888
+domeny@ConsultingService.pl
+
+WHOIS displays data with a delay not exceeding 15 minutes in relation to the .pl Registry system
+Registrant data available at http://dns.pl/cgi-bin/en_whois.pl
