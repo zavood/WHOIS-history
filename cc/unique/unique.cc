96e2bc8fa12ff4f5fce13fc06992326c542482c6
xml <xml@sandbox-test1.(none)>
2011-09-14 23:25:42 -0400

    auto checkin

---
+++ cc/unique/unique.cc
@@ -0,0 +1,118 @@
+
+Whois Server Version 1.0
+
+Domain names can now be registered with many different competing registrars.
+Go to http://registrar.verisign-grs.com/whois/ for detailed information.
+
+   Domain Name: UNIQUE.CC
+   Registrar: ENOM, INC.
+   Whois Server: whois.enom.com
+   Referral URL: http://www.enom.com
+   Name Server: NS2.NEWWORLDHOSTING.COM
+   Name Server: NS1.NEWWORLDHOSTING.COM
+   Status: CLIENT-XFER-PROHIBITED
+   Updated Date: 13-aug-2008
+   Creation Date: 16-oct-1998
+   Expiration Date: 16-oct-2011
+
+
+&gt;&gt;&gt; Last update of whois database: Wed, 14 Sep 2011 19:23:05 EDT &lt;&lt;&lt;
+
+NOTICE: The expiration date displayed in this record is the date the
+registrar's sponsorship of the domain name registration in the registry is
+currently set to expire. This date does not necessarily reflect the
+expiration date of the domain name registrant's agreement with the
+sponsoring registrar.  Users may consult the sponsoring registrar's
+Whois database to view the registrar's reported date of expiration
+for this registration.
+
+TERMS OF USE: You are not authorized to access or query our Whois
+database through the use of electronic processes that are high-volume and
+automated except as reasonably necessary to register domain names or
+modify existing registrations; the Data in VeriSign's (&quot;VeriSign&quot;) Whois
+database is provided by VeriSign for information purposes only, and to
+assist persons in obtaining information about or related to a domain name
+registration record. VeriSign does not guarantee its accuracy.
+By submitting a Whois query, you agree to abide by the following terms of
+use: You agree that you may use this Data only for lawful purposes and that
+under no circumstances will you use this Data to: (1) allow, enable, or
+otherwise support the transmission of mass unsolicited, commercial
+advertising or solicitations via e-mail, telephone, or facsimile; or
+(2) enable high volume, automated, electronic processes that apply to
+VeriSign (or its computer systems). The compilation, repackaging,
+dissemination or other use of this Data is expressly prohibited without
+the prior written consent of VeriSign. You agree not to use electronic
+processes that are automated and high-volume to access or query the
+Whois database except as reasonably necessary to register domain names
+or modify existing registrations. VeriSign reserves the right to restrict
+your access to the Whois database in its sole discretion to ensure
+operational stability.  VeriSign may restrict or terminate your access to the
+Whois database for failure to abide by these terms of use. VeriSign
+reserves the right to modify these terms at any time.
+
+The Registry database contains ONLY .cc, .tv, and .jobs domains
+and Registrars.
+=-=-=-=
+Visit AboutUs.org for more information about unique.cc
+&lt;a href=&quot;http://www.aboutus.org/unique.cc&quot;&gt;AboutUs: unique.cc&lt;/a&gt;
+
+
+Domain name: unique.cc
+
+Registrant Contact:
+   Unique Properties, Inc
+   Gail Newton ()
+
+   Fax:
+   59 East 14th Avenue
+   Eugene, OR 97401
+   US
+
+Administrative Contact:
+   Unique Properties, Inc
+   Gail Newton (domain@unique.cc)
+   +1.5414849381
+   Fax: na
+   59 East 14th Avenue
+   Eugene, OR 97401
+   US
+
+Technical Contact:
+   Unique Properties, Inc
+   Gail Newton (domain@unique.cc)
+   +1.5414849381
+   Fax: na
+   59 East 14th Avenue
+   Eugene, OR 97401
+   US
+
+Status: Locked
+
+Name Servers:
+   ns1.newworldhosting.com
+   ns2.newworldhosting.com
+
+Creation date: 16 Oct 1998 00:00:00
+Expiration date: 16 Oct 2011 04:00:00
+
+
+
+Get Noticed on the Internet!  Increase visibility for this domain name by listing it at www.whoisbusinesslistings.com
+=-=-=-=
+The data in this whois database is provided to you for information
+purposes only, that is, to assist you in obtaining information about or
+related to a domain name registration record. We make this information
+available &quot;as is,&quot; and do not guarantee its accuracy. By submitting a
+whois query, you agree that you will use this data only for lawful
+purposes and that, under no circumstances will you use this data to: (1)
+enable high volume, automated, electronic processes that stress or load
+this whois database system providing you this information; or (2) allow,
+enable, or otherwise support the transmission of mass unsolicited,
+commercial advertising or solicitations via direct mail, electronic
+mail, or by telephone. The compilation, repackaging, dissemination or
+other use of this data is expressly prohibited without prior written
+consent from us.
+
+We reserve the right to modify these terms at any time. By submitting
+this query, you agree to abide by these terms.
+Version 6.3 4/3/2002
